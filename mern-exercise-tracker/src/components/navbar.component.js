3fb436bbc7cad9fd483e677fbc058b27d492bb3e
Ameya <ameyaingale1@gmail.com>
2020-07-10 13:40:55 -0700

    Solve compiler err components

--- mern-exercise-tracker/src/components/navbar.component.js
+++ mern-exercise-tracker/src/components/navbar.component.js
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom';
 export default class Navbar extends Component {
     render() {
         return(
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
                 <Link to="/" className="navbar-brand">Exercise Tracker</Link>
                 <div class="collapse navbar-collapse">
                     <ul class="navbar-nav mr-auto">
