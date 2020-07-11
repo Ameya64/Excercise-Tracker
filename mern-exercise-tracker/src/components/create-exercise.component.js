ff36550d9f8ad49fe9cdb417563d798a1926c16b
Ameya <ameyaingale1@gmail.com>
2020-07-11 11:14:38 -0700

    Update create-exercise.component.js

--- mern-exercise-tracker/src/components/create-exercise.component.js
+++ mern-exercise-tracker/src/components/create-exercise.component.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import DatePicker from 'react-datepicker';
 export default class CreateExercise extends Component {
     constructor(props) {
         super(props)
